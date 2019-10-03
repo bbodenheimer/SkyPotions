6be7aa53b17deaa44beba1dba1b331cb936cc71a
Allie Craig <alliemakesthings@gmail.com>
2019-10-02 20:03:59 -0600

    Initial Commit

---
+++ backend/src/main/java/com/bbodenheimer/skyrimpotions/SkyrimPotionsApplication.java
@@ -0,0 +1,12 @@
+package com.bbodenheimer.skyrimpotions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyrimPotionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyrimPotionsApplication.class, args);
+	}
+}
